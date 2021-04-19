dc931e2d73d2a380b5c4c13e32f0c04395e6d791
DimaKulikov <dmyukulikov@gmail.com>
2021-04-19 15:41:01 +0300

    bugfix: image popup close animation

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -19,24 +19,21 @@ function ImagePopup(props) {
   }, [card, closeHandler])
 
   return (
-    <div className={`popup popup_image ${card ? 'popup_opened' : ''}`}>
-      <div className="popup__container">
-        <button
-          onClick={closeHandler}
-          className="popup__close-btn"
-          type="button"
-          aria-label="закрыть">
-        </button>
-        <img
-          className="popup__image"
-          // 
-          src={card?.link}
-          alt={card?.name}
-        />
-        <p className="popup__subtitle">{card?.name}</p>
-      </div>
+    <div className={`popup popup_image ${card ? "popup_opened" : ""}`}>
+      {card && (
+        <div className="popup__container">
+          <button
+            onClick={closeHandler}
+            className="popup__close-btn"
+            type="button"
+            aria-label="закрыть"
+          ></button>
+          <img className="popup__image" src={card.link} alt={card.name} />
+          <p className="popup__subtitle">{card.name}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default ImagePopup
