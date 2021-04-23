5678bc4d00840e702ce5c0c022e2a0d838dfe29d
DimaKulikov <dmyukulikov@gmail.com>
2021-04-23 10:39:55 +0300

    feat: popup close on overlay click

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -10,7 +10,6 @@ function Popup(props) {
         onClose();
       }
     }
-
     if (isOpen) {
       document.addEventListener("keydown", closeOnEsc);
     }
@@ -19,14 +18,17 @@ function Popup(props) {
     };
   }, [isOpen, onClose]);
 
+  function handleClose(evt) {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-btn'))
+    onClose()    
+  }
   
   const popupClassName = `popup popup_${name} ${isOpen ? "popup_opened" : ""}`;
 
   return (
-    <div className={popupClassName}>
+    <div className={popupClassName} onClick={handleClose}>
       <div className="popup__container">
         <button
-          onClick={onClose}
           className="popup__close-btn"
           type="button"
           aria-label="закрыть"
