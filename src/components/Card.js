86b3c453efe0fe99e5fced1ed1e72e93f58e574a
Dima <dmyukulokov@gmail.com>
2021-04-25 16:54:04 +0300

    fix: remove state from Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
+// import { useState } from "react";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 import {CardLikeSpinner} from "./CardLikeSpinner";
 
