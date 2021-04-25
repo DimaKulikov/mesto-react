974de5fc5abf4ee50d1b31b5a3209d24decbeef6
Dima <dmyukulokov@gmail.com>
2021-04-25 16:57:55 +0300

    fix: remove card like spinner

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,13 @@
 import { useContext } from "react";
-// import { useState } from "react";
 import CurrentUserContext from "../contexts/CurrentUserContext";
-import {CardLikeSpinner} from "./CardLikeSpinner";
 
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = useContext(CurrentUserContext);
-
-  // const [likeProcessing, setLikeProcessing] = useState(false)
 
   const isOwn = currentUser._id === card.owner._id;
   const isLiked = card.likes.some((i) => i._id === currentUser._id);
   const likeBtnClassName = `card__like-btn 
     ${isLiked ? "card__like-btn_active" : ""}
-    ${/*likeProcessing ? "card__like-btn_disabled" : */""}
   }`;
 
   function handleImageClick() {
@@ -20,12 +15,8 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   }
 
   function handleLikeClick() {
-    // setLikeProcessing(true)
     onCardLike(card)
       .catch(console.error)
-      .finally(() => {
-        // setLikeProcessing(false)
-      });
   }
 
   function handleDeleteClick() {
@@ -49,17 +40,12 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
       <div className="card__caption">
         <h2 className="card__title">{card.name}</h2>
         <div className="card__like-container">
-          {
-            // likeProcessing ? <CardLikeSpinner/> : 
-            <button
-              onClick={handleLikeClick}
-              className={likeBtnClassName}
-              type="button"
-              aria-label="лайк"
-            ></button>
-          }
-          
-          
+          <button
+            onClick={handleLikeClick}
+            className={likeBtnClassName}
+            type="button"
+            aria-label="лайк"
+          ></button>
           <span className="card__like-count">{card.likes.length}</span>
         </div>
       </div>
